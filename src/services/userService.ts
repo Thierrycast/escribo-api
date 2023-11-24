6f8824912e63a17cc3529a79861270d94833393d
Thierry Castro <thierrycastro555@gmail.com>
2023-11-24 12:06:31 -0300

    feat(userservice): add password encryption

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt'
 import { type CreateUserDTO, type OutputUserDTO } from '../dtos/user.dto'
 import { ApiError } from '../helpers/api-error'
 import userModel from '../models/user.model'
@@ -5,11 +6,14 @@ import userModel from '../models/user.model'
 async function create (data: CreateUserDTO): Promise<OutputUserDTO> {
   const emailExists = await userModel.verifyEmail(data.email)
 
-  if (emailExists != null) {
-    throw new ApiError('E-mail já existente', 409)
+  if (emailExists != null) throw new ApiError('E-mail já existente', 409)
+
+  const formattedData = {
+    ...data,
+    senha: bcrypt.hashSync(data.senha, 10)
   }
 
-  const { nome, email, senha, ...user } = await userModel.create(data)
+  const { nome, email, senha, ...user } = await userModel.create(formattedData)
 
   return user
 }
